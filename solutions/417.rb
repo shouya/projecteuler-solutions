16cbd1be2ff89b6ef7f66c7a1b6ce8aa71232f4d
Shou Ya <zxyzxy12321@gmail.com>
2013-03-08 11:52:07 +0800

    still solving prob 417

--- solutions/417.rb
+++ solutions/417.rb
@@ -1,6 +1,8 @@
 #
 
 require_relative 'ext_lib'
+#require 'ruby-prof'
+
 
 sum = 0
 
@@ -15,33 +17,41 @@ def recur_cycle(n)
     # the following line could be diminished
     # return 0 if r == 0
 
-    r *= 10 until r >= n
-    if p = a.index(r)
-      return a.length - p
-    end
+    r *= 10
+    return a.length - a.index(r) if a.index(r)
     a << r
   end
   return 0
 end
 
+p recur_cycle(49)
+
+
+__END__
 
 sum = 0
-
-3.upto(1_000_000) do |n|
+#RubyProf.start
+3.upto 1_000 do |n|
   puts n if n % 1000 == 0
-  while n % 2 == 0 or n % 5 == 0
-    n /= n % 2 == 0 ? 2 : 5
-  end
+  n /= 2 while n % 2 == 0
+  n /= 5 while n % 5 == 0
+
   next if n == 1
 
   if p = @recur_tbl.binary_search_assoc(n)
     sum += @recur_tbl[p][1]
   else
-    @recur_tbl << (v = recur_cycle(n))
+    v = recur_cycle(n)
+    @recur_tbl << [n, v] unless n > 3000
     sum += v
   end
 
 end
 puts sum
+#result = RubyProf.stop
 
 
+# Print a graph profile to text
+
+#printer = RubyProf::FlatPrinterWithLineNumbers.new(result)
+#printer.print(STDOUT, {})
