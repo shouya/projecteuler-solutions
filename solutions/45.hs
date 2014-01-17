cc65597a2ff0112bae8d1faf7638e2426a6258c5
Shou <shouya@users.noreply.github.com>
2014-01-17 13:30:59 +0800

    Work lost for problem 58
    While the result is printed out, wish to save it in a text file:
          $ cat > 58.hs
          ^C
          $ cat 58.hs
          $
    
    Below is all code snippets I can recover.
    
    snd $ find (\((np,n),_) -> np/n < 0.1) (result primeList)
    
    spiral = [(floor $ ((sqrt (fromIntegral n - 1))+1)/2+1, n) | n <- [1..]]

--- solutions/45.hs
+++ solutions/45.hs
@@ -4,7 +4,7 @@ isInt :: Float -> Bool
 isInt x = 0.00001 > abs (x - fromInteger (round x))
 
 main = do
-	putStrLn $ show $ take 5 [ (truncate $ (a*a+a) / 2) |
+	putStrLn . show $ take 5 [ (truncate $ (a*a+a) / 2) |
 			a <- [1..],
 			isInt ((1+a)/2) &&
 			isInt (1/6*(1+sqrt (1+12*a+12*a*a)))]
